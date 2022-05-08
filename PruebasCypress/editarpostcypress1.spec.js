1a0f4a539daf4f0c94894192274fc9d3207866f7
Hawer FORERO REY <hawerforero@gmail.com>
2022-05-07 22:22:25 -0500

    add escenario positivo editar post cypress

---
+++ PruebasCypress/editarpostcypress1.spec.js
@@ -0,0 +1,48 @@
+describe('Testing positivo editar post', () => {
+    beforeEach(()=>{
+       cy.visit('http://localhost:2368/ghost/#/signin')
+        cy.wait(3000)
+    })
+
+    it('Editar post', ()=>{
+
+        //Login
+        cy.get("#ember8").type("h.foreror@uniandes.edu.co");
+        cy.get("#ember10").type("admin12345");
+        cy.wait(1000);
+        cy.get("#ember12").click();
+        cy.wait(1000);
+
+        //Open dashboard
+        cy.url().should("include", "/site");
+        cy.wait(1000);
+
+        //Posts
+        cy.contains("Posts").click();
+        cy.url().should("include", "/posts");
+        cy.wait(1000);
+
+        //Crete post
+        cy.contains("New post").click();
+        let body = cy.get("body");
+        body.find("textarea").first().type("PostPrueba2", { force: true });
+        cy.get(".gh-koenig-editor-pane").first().click();
+        cy.wait(2000);
+
+        //Posts
+        cy.contains("Posts").click();
+        cy.url().should("include", "/posts");
+        cy.wait(1000);
+
+        //Select post
+        cy.contains("PostPrueba2").click({ force: true });
+
+        //Edit post
+        body = cy.get("body");
+        body.find("textarea").first().type("Post Modificado");
+        cy.contains("Preview").click();
+        cy.wait(500);
+
+    })
+
+  })
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PruebasE2E
+# PruebasE2E Automatizadas
+- Hawer Alberto Forero Rey 
+- John Edwin Guerrero Ayala
+- Brayan Sebastian Hernandez Barrera
+- Jhon Sebastian Bohorquez Guerrero
+
+# Descripción de escenarios
+| Descripción                        | Responsable      |  Cypress |Kraken |
+|------------------------------------|------------------|-------------|-----|
+| Escenario positivo editar post  | Hawer Forero |  editarpostcypress1.spec.js | |
