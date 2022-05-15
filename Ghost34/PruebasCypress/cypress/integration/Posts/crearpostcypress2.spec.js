096830bec2082716ef2a769a09268fbaabc887df
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-05-15 00:58:11 -0500

    Prueba en cypress crearpostcypress2 completa

---
+++ Ghost34/PruebasCypress/cypress/integration/Posts/crearpostcypress2.spec.js
@@ -0,0 +1,62 @@
+describe('Testing positivo crear post', () => {
+  beforeEach(()=>{
+    cy.fixture('admin').as('adminData')
+    cy.fixture('site').then((siteData) => {
+      cy.visit(siteData.url);
+      cy.wait(1000);
+    })
+  })
+
+  it('Crear post', () => {
+
+    // Login
+    cy.get('@adminData').then(adminData => {
+      cy.get('[type="email"]').type(adminData.email);
+      cy.get('[type="password"]').type(adminData.password);
+      cy.screenshot();
+      cy.get('[type="submit"]').click();
+      cy.wait(1000);
+    })
+
+    //Open dashboard
+    cy.url().should("include", "/site");
+    cy.screenshot();
+    cy.wait(1000);
+
+    //Posts
+    cy.get('.gh-nav-top').contains('Posts').click();
+    cy.url().should("include", "/posts");
+    cy.screenshot();
+    cy.wait(1000);
+    
+    //Create post
+    cy.get('.post-header').contains('New post').click();
+    cy.screenshot();
+    cy.wait(1000);
+    let body = cy.get("body");
+    body.find("textarea").first().type("Prueba publicar post", { force: true });
+    cy.get(".gh-koenig-editor-pane").first().click();
+    cy.screenshot();
+    cy.wait(5000);
+
+    //Publish post
+
+    cy.get('.gh-editor-header > section').contains('Publish').click();
+    cy.screenshot();
+    cy.wait(1000);
+    cy.get('body > div > div > footer').contains('Publish').click();
+    cy.screenshot();
+    cy.wait(1000);
+    cy.get(".gh-koenig-editor-pane").first().click();
+    cy.screenshot();
+    cy.wait(5000);
+    cy.get('.gh-editor-header > div > div > a').click();
+    cy.screenshot();
+    cy.wait(1000);
+    
+    //Validate draft post
+    cy.get('.gh-posts-list-item').contains('Published').first().focus();
+    cy.screenshot();
+    cy.get('.gh-posts-list-item').contains('Published').first().parent().contains("Prueba publicar post").should('be.visible');
+  })
+})
