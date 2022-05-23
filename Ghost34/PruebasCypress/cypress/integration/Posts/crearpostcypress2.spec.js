ea72aa9eeb7c63d6bfc994591ce90d928e0c26e1
Sebastian Bohorquez <sebastian.bohorquez@ariadnacg.com>
2022-05-22 21:53:31 -0500

    Escenarios de crear post

--- Ghost34/PruebasCypress/cypress/integration/Posts/crearpostcypress2.spec.js
+++ Ghost34/PruebasCypress/cypress/integration/Posts/crearpostcypress2.spec.js
@@ -1,3 +1,5 @@
+const { faker } = require('@faker-js/faker');
+
 describe('Testing positivo crear post', () => {
   beforeEach(()=>{
     cy.fixture('admin').as('adminData')
@@ -9,54 +11,46 @@ describe('Testing positivo crear post', () => {
 
   it('Crear post', () => {
 
+    const tupla = [faker.datatype.string(10)];
+
     // Login
     cy.get('@adminData').then(adminData => {
       cy.get('[type="email"]').type(adminData.email);
       cy.get('[type="password"]').type(adminData.password);
-      cy.screenshot();
       cy.get('[type="submit"]').click();
       cy.wait(1000);
     })
 
     //Open dashboard
     cy.url().should("include", "/site");
-    cy.screenshot();
     cy.wait(1000);
 
     //Posts
     cy.get('.gh-nav-top').contains('Posts').click();
     cy.url().should("include", "/posts");
-    cy.screenshot();
     cy.wait(1000);
     
     //Create post
     cy.get('.post-header').contains('New post').click();
-    cy.screenshot();
     cy.wait(1000);
     let body = cy.get("body");
-    body.find("textarea").first().type("Prueba publicar post", { force: true });
+    body.find("textarea").first().type(tupla[0], {force: true, parseSpecialCharSequences: false});
     cy.get(".gh-koenig-editor-pane").first().click();
-    cy.screenshot();
     cy.wait(5000);
 
     //Publish post
 
     cy.get('.gh-editor-header > section').contains('Publish').click();
-    cy.screenshot();
     cy.wait(1000);
     cy.get('body > div > div > footer').contains('Publish').click();
-    cy.screenshot();
     cy.wait(1000);
     cy.get(".gh-koenig-editor-pane").first().click();
-    cy.screenshot();
     cy.wait(5000);
     cy.get('.gh-editor-header > div > div > a').click();
-    cy.screenshot();
     cy.wait(1000);
     
     //Validate publish post
     cy.get('.gh-posts-list-item').contains('Published').first().focus();
-    cy.screenshot();
-    cy.get('.gh-posts-list-item').contains('Published').first().parent().contains("Prueba publicar post").should('be.visible');
+    cy.get('.gh-posts-list-item').contains('Published').first().parent().contains(tupla[0]).should('be.visible');
   })
 })
